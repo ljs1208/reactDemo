69bba19cd787ca53a83192b333caf979a5e6362f
jasonlu <jjlu521016@163.com>
2018-01-25 13:33:40 +0800

    测试提交，Router测试

--- src/router/router.js
+++ src/router/router.js
@@ -4,18 +4,20 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 import Home from '../pages/Home/Home';
 import Page1 from '../pages/Page1/Page1';
-
+import Page2 from '../pages/Page2/Page2';
 
 const getRouter = () => (
     <Router>
         <div>
             <ul>
                 <li><Link to="/">首页</Link></li>
-                <li><Link to="/page1">Page1</Link></li>
+                <li><Link to="/page1">页面1</Link></li>
+                <li><Link to="/page2">页面2</Link></li>
             </ul>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/page1" component={Page1}/>
+                <Route path="/page2" component={Page2}/>
             </Switch>
         </div>
     </Router>
